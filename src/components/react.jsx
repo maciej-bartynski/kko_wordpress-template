06013bdbe3b392789666285f5f878e37a0a7845b
JestemBartek <maciek.bartynski@gmail.com>
2018-09-27 20:15:21 +0200

    set up finished

--- src/components/react.jsx
+++ src/components/react.jsx
@@ -1,10 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const Compo = () => {
-    return <h1>hello in there</h1>;
-}
-
-ReactDOM.render(
-    <Compo />, document.querySelector('.root')
-);
+import  './DesktopButton/index.jsx';
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import './index.scss';
+import 'materialize-css/dist/js/materialize.min.js';
 import './components/react.jsx';
 import './scripts/alert.js';
